4ede5f21dc0818c955487f9c74aa5655121d5721
maartenputman <r0942539@ucll.be>
2024-12-19 13:40:13 +0100

    added UserInput, added length field to BookInput

--- back-end/types/index.ts
+++ back-end/types/index.ts
@@ -3,9 +3,18 @@ type BookInput = {
     id?: number,
     title: string,
     author: string,
+    length: number,
     synopsis: string
+};
+
+type UserInput = {
+    id?: number,
+    username: string,
+    password: string,
+    books?: BookInput[]
 };
 
 export {
     BookInput,
+    UserInput,
 }
